ca5ea06c89216ed0f5e4d876b050a9f654cb31ab
Shy <Shy@DESKTOP-N3KQ21T>
2023-02-19 01:41:57 +1100

    Добавьте файлы проекта.

---
+++ CopyGroup_Lab2/CopyGroup_Lab_2.cs
@@ -0,0 +1,100 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Group = Autodesk.Revit.DB.Group;
+
+namespace CopyGroup_Lab2
+{
+    [TransactionAttribute(TransactionMode.Manual)]
+    public class CopyGroup_Lab_2 : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            try
+            {
+                UIDocument uiDoc = commandData.Application.ActiveUIDocument;
+                Document doc = uiDoc.Document;
+                GroupPickFilter groupPickFilter = new GroupPickFilter();
+                Reference reference = uiDoc.Selection.PickObject(ObjectType.Element, groupPickFilter, "Выберите группу");
+                Element element = doc.GetElement(reference);
+                Group group = element as Group;
+                XYZ point = uiDoc.Selection.PickPoint("Выберите точку вставки");
+                XYZ groupCenter = GetCenter(group);
+                Room room = GetRoomByPoint(doc, groupCenter);
+                XYZ roomCenter = GetCenter(room);
+                XYZ offset = roomCenter - groupCenter;
+
+                Room selectedRoom = GetRoomByPoint(doc, point);
+                XYZ pointToPlace = GetCenter(selectedRoom) - offset;
+
+
+                Transaction transaction = new Transaction(doc);
+                transaction.Start("copy group");
+                doc.Create.PlaceGroup(pointToPlace, group.GroupType);
+                transaction.Commit();
+            }
+            catch (Autodesk.Revit.Exceptions.OperationCanceledException)
+            {
+                return Result.Cancelled;
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+                return Result.Failed;
+            }
+
+            return Result.Succeeded;
+
+        }
+        public XYZ GetCenter(Element element)
+        {
+            BoundingBoxXYZ bbox = element.get_BoundingBox(null);
+            return (bbox.Max + bbox.Min) / 2;
+        }
+        public Room GetRoomByPoint(Document doc, XYZ point)
+        {
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+            collector.OfCategory(BuiltInCategory.OST_Rooms);
+            foreach (Element e in collector)
+            {
+                Room room = e as Room;
+                if (room != null)
+                {
+                    if (room.IsPointInRoom(point))
+                    {
+                        return room;
+                    }
+                }
+            }
+            return null;
+        }
+    }
+    public class GroupPickFilter : ISelectionFilter
+    {
+        public bool AllowElement(Element elem)
+        {
+            if (elem.Category.Id.IntegerValue == (int)BuiltInCategory.OST_IOSModelGroups)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool AllowReference(Reference reference, XYZ position)
+        {
+            return false;
+        }
+    }
+}
